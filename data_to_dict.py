e73c0d06fec49424a9f0b5bdfdd2d369d5abbbaf
clementboloch <clement.boloch@gmail.com>
2023-02-24 10:59:04 +0100

    updates str_to_float function and convert function

--- data_to_dict.py
+++ data_to_dict.py
@@ -1,6 +1,26 @@
 import pandas as pd
 
+columnsName = ['name', 'price', 'profit']
+
+
+def str_to_float(val):
+    if isinstance(val, (float, int)):
+        return val
+    val = val.replace(u'\xa0', u'')
+    val = val.replace(u' ', u'')
+    if '%' in val:
+        val = val.replace(u'%', u'')
+        val = float(val)
+        val /= 100
+    else:
+        val = float(val)
+    print(val)
+    return val
+
 
 def convert(data):
-    excel_data_df = pd.read_excel(data)
-    return excel_data_df.to_dict(orient='records')
+    df = pd.read_excel(data)
+    df.columns = columnsName
+    for column in ['price', 'profit']:
+        df[column] = df[column].apply(lambda x: str_to_float(x))
+    return df.to_dict(orient='index')
