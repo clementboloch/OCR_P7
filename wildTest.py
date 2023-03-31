ce7d00fc9dd31318a6baff4ad3c59cc91c28eec0
clementboloch <clement.boloch@gmail.com>
2023-03-31 16:28:47 +0200

    deletes wildTest.py file

--- wildTest.py
+++
@@ -1,7 +0,0 @@
-import potential
-
-potRes1 = potential.getWay("data/dataset1_Python+P7.csv", 500, csv=True)
-print('Potential1', potRes1)
-
-potRes2 = potential.getWay("data/dataset2_Python+P7.csv", 500, csv=True)
-print('Potential2', potRes2)
